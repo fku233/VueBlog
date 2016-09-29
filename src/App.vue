2e365ab19f4caf257bfc5eca4022675bf4e85c4f
x-pengg <i@ridog.me>
2016-09-29 13:32:52 +0800

    增加样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
+  <div id="main">
+    <my-header></my-header>
     <router-view></router-view>
+    <vue-progress-bar></vue-progress-bar>
   </div>
 </template>
+
+<style>
+  body{
+    font-family: 'Microsoft JhengHei', '微軟正黑體', 'Microsoft YaHei', '微软雅黑', Tahoma, Verdana, sans-serif;
+    font-size: 14px;
+  }
+
+  a{
+    text-decoration: none;
+    outline: none;
+    color: #2B2B2B;
+  }
+
+  #main{
+    width: 50%;
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 700px) {
+    #main{
+      width: 100%;
+    }
+  }
+  @media screen and (max-width: 1000px) {
+    #main{
+      width: 90%;
+    }
+  }
+</style>
+
+<script>
+  import Header from './components/Header.vue'
+  export default{
+    components: {
+      'my-header': Header
+    }
+  }
+</script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="articles">
     <h2 v-if="!items.length">载入中...</h2>
     <ul>
       <li v-for="item in items">
@@ -26,6 +26,7 @@
     },
     methods:{
       setItems(){
+        this.$Progress.start();
         this.$http.get(API,{
           headers: {
             "Content-Type": "application/x-www-form-urlencoded"
@@ -33,8 +34,10 @@
         })
         .then((response) => {
           this.items = response.data;
+          this.$Progress.finish();
         })
         .catch(function(response) {
+          this.$Progress.fail();
           console.log(response)
         })
       }
@@ -44,5 +47,17 @@
 </script>
 
 <style>
+
+  ul,ol{
+    list-style: none;
+  }
+
+  #articles ul li{
+    padding: 1em;
+    border-bottom: 1px solid #eee;
+    line-height: 4;
+    font-size: 1.6em;
+    font-weight: 600;
+  }
 
 </style>
